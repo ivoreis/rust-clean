a96640018fd95d6c7d6edc4414286bc3e175288c
Ivo Reis <1410207+ivoreis@users.noreply.github.com>
2023-03-18 09:34:48 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use actix_clean_architecture::create_app::create_app;
+use actix_web::HttpServer;
+
+#[cfg(test)]
+mod tests;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let server = HttpServer::new(move || create_app())
+        .workers(4)
+        .bind(("127.0.0.1", 8080))?;
+    server.run().await
+}
